70cb59a8bb108258504b8fe3866f65754cff72a3
Eder Gracional <Emailedergacional@hotmail.com>
2022-03-07 11:07:59 -0300

    52. Estrutura do Projeto

---
+++ src/views/examples/About.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const About = props => (
+    <div className="Home">
+        <h1>Sobre</h1>
+        <p>O nosso sistema foi criado...</p>
+    </div>
+)
+
+export default About
